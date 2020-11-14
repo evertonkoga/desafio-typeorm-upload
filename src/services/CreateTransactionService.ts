c5ec9ec549a3bf8f9ef6bf7a4e355964f6cad911
Everton Koga <everton.koga@sotran.com.br>
2020-11-14 08:00:32 -0300

    commit inicial

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (!['income', 'outcome'].includes(type))
+      throw new AppError('Type not allow.');
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total)
+      throw new AppError('Transaction not allow.');
+
+    let categoryResult = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryResult) {
+      categoryResult = categoriesRepository.create({ title: category });
+
+      await categoriesRepository.save(categoryResult);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryResult.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
